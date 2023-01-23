83e6153e72f665082910c30f071af414aea03f2b
TejsinghDhaosriya <tejsinghdhaosriya@gmail.com>
2023-01-23 16:23:46 +0530

    timesheet service, test and docker-compose setup

---
+++ TimesheetService/Controllers/ProjectController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TimesheetService.Controllers
+{
+    [Route("api/v1/project")]
+    [ApiController]
+    public class ProjectController : ControllerBase
+    {
+    }
+}
