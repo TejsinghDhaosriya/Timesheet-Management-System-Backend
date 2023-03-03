76d073f8d997b52d1db848c99c67615c9b55853c
TejsinghDhaosriya <tejsinghdhaosriya@gmail.com>
2023-03-03 16:32:41 +0530

    keycloak integration with api gateway

--- src/main/ApiGateway/Program.cs
+++ src/main/ApiGateway/Program.cs
@@ -1,3 +1,5 @@
+using JwtAuthenticationManager;
+using Microsoft.AspNetCore.Authentication;
 using Ocelot.DependencyInjection;
 using Ocelot.Middleware;
 
@@ -7,6 +9,16 @@ builder.Configuration.SetBasePath(Directory.GetCurrentDirectory())
     .AddEnvironmentVariables();
 
 builder.Services.AddOcelot(builder.Configuration);
+
+//Key Cloak Auth
+const string publicKey =
+    "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAqjpWuE2D3Qp2U6v1CZYDFazKa36uNY8vylAOoiztAUARtf1CVZm8iM497PM+kBVlLkPOjiQtWwQ7zoLiqdn3ERiNfheQg8FL6DVSuz6ud1BciNADhhN+du6l2OBnXOV8m3sDBLg6RFdXW2od4TQy2z3ekB9MhF1zfibF86g6ZVXkQ9gAq8A0qqIVj2T+IaIJVhyHaap5AdoJzydlkvXySmoBQKNp/F/AU87U6sT8iRC/atvKAGKqQUIhJRvmOgW7pdjifu90Mn8iFVtOvXAeEK9vo/H4zlwz+pUXDnY8ZmYAdX1t/SWpQ/IEznaER8mmzXPZ0fzitb9tKLBaUVWnHwIDAQAB";
+const bool isStaging = true;
+const string clientName = "TMS-Client";
+const string issuer = "https://143.110.248.171:8443/realms/Augmento";
+
+builder.Services.AddKeyCloakJwtAAuthExtension(publicKey, issuer, isStaging);
+builder.Services.AddTransient<IClaimsTransformation>(_ => new ClaimsTransformer(clientName));
 builder.Services.AddCors(options =>
 {
     options.AddPolicy("CorsPolicy",
@@ -21,5 +33,7 @@ app.UseHttpsRedirection();
 app.UseCors("CorsPolicy");
 
 await app.UseOcelot();
+
+app.UseAuthorization();
 
 app.Run();
